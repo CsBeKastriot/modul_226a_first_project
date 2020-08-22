e057bc570b84c5d09cc69e8df9e2bcac4e0385af
CsBeKastriot <Kastriot.Dulla@CsBe.ch>
2020-08-22 10:41:33 +0200

    Finished AB01 including the checks for the Mastermind Game and started AB02

--- src/main/java/ab01/MastermindAB01.java
+++ src/main/java/ab01/MastermindAB01.java
@@ -1,26 +1,29 @@
 package ab01;
 
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class MastermindAB01 {
 
     public static void main(String[] args) {
-        GameStart();
+        gameStart();
     }
 
-    public static void GameStart() {
-        System.out.println("Geben Sie einen Versuchscode mit vier Buchstaben aus der Menge {r,g,b,y,w} ein:");
+    public static void gameStart() {
+        System.out.println("Geben Sie einen Versuchscode mit vier Buchstaben aus der Menge {r,g,b,y,s,w} ein:");
         Scanner scan = new Scanner(System.in);
         String userInput = scan.next();
 
-        if(userInput.equals("") || userInput.equals(" ")) {
-            System.out.println("Kein gültiger Inut!");
-            GameStart();
-        } else if(userInput.matches("\\d")) {
-            System.out.println("Bitte geben Sie einen der oben erwähnten Buchstaben ein");
-            GameStart();
-        } else if(userInput.matches("r"+"g"+"b"+"y"+"w")) {
-            String gameEnding = "Spiel beendet. Ihr geheimcode war " + String.format(userInput)+ ". Anzahl versuche: 5";
+        // Checks userInput to match the following patters.
+        // To learn more about regex see the following link: https://www.javatpoint.com/java-regex
+        if(Pattern.matches("\\s+", userInput) || Pattern.matches("\\W+", userInput)) {
+            System.out.println("Kein gültiger Inut!\n");
+            gameStart();
+        } else if(Pattern.matches("[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)", userInput) || Pattern.matches("\\W+", userInput)) {
+            System.out.println("Bitte geben Sie einen der oben erwähnten Buchstaben ein.\n");
+            gameStart();
+        } else if(Pattern.matches("[a-z&&[rgbysw]]{1,2}", userInput)) {
+            String gameEnding = "Spiel beendet. Ihr geheimcode war " + userInput+ ". Anzahl versuche: 5";
             System.out.println(gameEnding);
         }
     }
---
+++ src/main/java/ab02/AB02.java
@@ -0,0 +1,9 @@
+package ab02;
+
+public class AB02 {
+    public static void main(String[] args){
+
+    }
+
+
+}
